f085f38d6c94148380e3cd54b623cb3d7e5323b2
Valeriy Kuzin <v.kuzin@nixsolutions.com>
2017-09-04 17:37:09 +0300

    Clean

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   before_action :require_user
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-  before_action :google_sheets_service, only: [:create, :update, :destroy]
+  before_action :google_sheets_service, only: [:create, :update]
 
   # GET /products
   # GET /products.json
